6060e02ad03cc5b317e87d8b0096fc9d16666b81
Shreyash Khare <69174544+LaST0-0CoDe@users.noreply.github.com>
2023-03-17 14:49:06 +0530

    Update find_keywords.sh

--- find_keywords.sh
+++ find_keywords.sh
@@ -15,19 +15,6 @@ if [ -z "$url_file" ]; then
   echo "Please provide a URLs file using the -u flag"
   exit 1
 fi
-
-# Create a new file to store the found URLs
-found_urls_file="found_urls.txt"
-touch $found_urls_file
-
-# Start the animation loop
-while true; do
-    # Print the animation string with a message
-    printf "\r%s %s" "Your URL is loading" "${animation:i++%${#animation}:1}"
-    # Delay for a short time to slow down the animation
-    sleep 0.1
-done & # Run the loop in the background
-
 # ASCII art
 cat << "EOF"
 
@@ -41,6 +28,18 @@ cat << "EOF"
 ⠀⠀⠀⠀⠀⠈⠙⠦⢕⡋⠶⠄⣤⠤⠤⠤⠤⠂⡠⠀⡇
 
 EOF
+
+# Create a new file to store the found URLs
+found_urls_file="found_urls.txt"
+touch $found_urls_file
+
+# Start the animation loop
+while true; do
+    # Print the animation string with a message
+    printf "\r%s %s" "Your URL is loading" "${animation:i++%${#animation}:1}"
+    # Delay for a short time to slow down the animation
+    sleep 0.1
+done & # Run the loop in the background
 
 # Loop through the URLs file and search for the specified keywords
 while read url; do
