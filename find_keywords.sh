f6a7f083cf1ee348de9529f87a6c8e5706ba1cb2
Shreyash Khare <69174544+LaST0-0CoDe@users.noreply.github.com>
2023-03-17 12:59:50 +0530

    Update find_keywords.sh

--- find_keywords.sh
+++ find_keywords.sh
@@ -1,48 +1,27 @@
 #!/bin/bash
 
-# Parse command line arguments
-while getopts u: flag
-do
-    case "${flag}" in
-        u) url_file=${OPTARG};;
-    esac
+while getopts "u:" arg; do
+  case $arg in
+    u)
+      url_file=$OPTARG
+      ;;
+  esac
 done
 
-# Check if URL file is provided
-if [ -z "${url_file}" ]
-then
-    echo "Error: Please provide a file containing URLs using the -u flag."
-    exit 1
+if [ -z "$url_file" ]; then
+  echo "Please provide a URLs file using the -u flag"
+  exit 1
 fi
 
-# Define keywords to search for
-keywords=(q= s= search= id= lang= keyword= query= page= keywords= year= view= email= type= name= p= month= immagine= list_type= url= terms= categoryid= key= l= begindate= enddate= categoryid2= t= cat= category= action= bukva= redirect_uri= firstname= c= lastname= uid= startTime= eventSearch= categoryids2= categoryids= sort= positiontitle= groupid= m= message= tag= pn= title= orgId= text= handler= myord= myshownums= id_site= city= search_query= msg= sortby= produkti_po_cena= produkti_po_ime= mode= CODE= location= v= order= n= term= start= k= redirect= ref= file= mebel_id= country= from= r= f= field5B5D= searchScope= state= phone= Itemid= lng= place= bedrooms= expand= e= price= d= path= address= day= display= a= error= form= language= mls= kw= u=
+# Create a new file to store the found URLs
+found_urls_file="found_urls.txt"
+touch $found_urls_file
 
-)
+# Loop through the URLs file and search for the specified keywords
+while read url; do
+  if echo "$url" | grep -E 'q|s|search|id|lang|keyword|query|page|keywords|year|view|email|type|name|p|month|immagine|list_type|url|terms|categoryid|key|l|begindate|enddate|categoryid2|t|cat|category|action|bukva|redirect_uri|firstname|c|lastname|uid|startTime|eventSearch|categoryids2|categoryids|sort|positiontitle|groupid|m|message|tag|pn|title|orgId|text|handler|myord|myshownums|id_site|city|search_query|msg|sortby|produkti_po_cena|produkti_po_ime|mode|CODE|location|v|order|n|term|start|k|redirect|ref|file|mebel_id|country|from|r|f|field5B5D|searchScope|state|phone|Itemid|lng|place|bedrooms|expand|e|price|d|path|address|day|display|a|error|form|language|mls|kw|u' > /dev/null; then
+    echo $url >> $found_urls_file
+  fi
+done < $url_file
 
-# Initialize list of URLs with keywords
-url_list=""
-
-# Read each URL from file and search for keywords
-while read url
-do
-    # Split URL by query string parameters
-    params=$(echo "${url}" | awk -F'[?&]' '{print substr($0,index($0,$2))}')
-
-    # Check if URL contains at least one keyword
-    if echo "${params}" | grep -q -E "${keywords[*]}"
-    then
-        url_list="${url_list}${url}\n"
-        # Search for each keyword in URL parameters
-        for keyword in "${keywords[@]}"
-        do
-            echo "${params}" | grep -o "${keyword}[^&]*" | sed "s/${keyword}//g" | while read value
-            do
-                echo "Keyword ${keyword} found in ${url} with value ${value}"
-            done
-        done
-    fi
-done < "${url_file}"
-
-# Output list of URLs with keywords
-echo -e "URLs with keywords:\n${url_list}"
+echo "Keyword found URLs saved in $found_urls_file"
