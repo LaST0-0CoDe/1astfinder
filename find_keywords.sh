af7a633102ea2c64d7e5025acf1a0a32c7633e02
Shreyash Khare <69174544+LaST0-0CoDe@users.noreply.github.com>
2023-03-17 12:41:46 +0530

    Update find_keywords.sh

--- find_keywords.sh
+++ find_keywords.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-# Function to extract keywords from URL
-extract_keywords() {
-    echo "$1" | grep -oE 'q|s|search|id|lang|keyword|query|page|keywords|year|view|email|type|name|p|month|immagine|list_type|url|terms|categoryid|key|l|begindate|enddate|categoryid2|t|cat|category|action|bukva|redirect_uri|firstname|c|lastname|uid|startTime|eventSearch|categoryids2|categoryids|sort|positiontitle|groupid|m|message|tag|pn|title|orgId|text|handler|myord|myshownums|id_site|city|search_query|msg|sortby|produkti_po_cena|produkti_po_ime|mode|CODE|location|v|order|n|term|start|k|redirect|ref|file|mebel_id|country|from|r|f|field5B5D|searchScope|state|phone|Itemid|lng|place|bedrooms|expand|e|price|d|path|address|day|display|a|error|form|language|mls|kw|u|\?s=|\?q=|/search\?q=|/search\?keyword=|/search\?search=|/search-results\?q=|/search\?field5B5D=|/videos\?tag=|/videos\?place=|/videos\?search=|/property/search\?uid=|/search\.php\?q='
+usage() {
+    echo "Usage: $0 -u <urls_file>"
 }
 
-# Check if urls file is provided as argument
-if [[ -z "$1" ]]; then
-    echo "Please provide a URLs file as argument using -u flag."
+if [ $# -eq 0 ]; then
+    usage
     exit 1
 fi
 
-# Check if urls file exists
-if [[ ! -f "$1" ]]; then
-    echo "The specified URLs file does not exist."
+while getopts "u:" opt; do
+    case ${opt} in
+        u )
+            urls_file=$OPTARG
+            ;;
+        \? )
+            usage
+            exit 1
+            ;;
+        : )
+            echo "Error: -$OPTARG requires an argument."
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$urls_file" ]; then
+    echo "Error: -u flag is missing."
+    usage
     exit 1
 fi
 
-# Loop through each URL in the file and extract keywords
-while read url; do
-    echo "Keywords in URL: $url"
-    keywords=$(extract_keywords "$url")
-    echo "$keywords"
-done < "$1"
+keywords=("q" "s" "search" "id" "lang" "keyword" "query" "page" "keywords" "year" "view" "email" "type" "name" "p" "month" "immagine" "list_type" "url" "terms" "categoryid" "key" "l" "begindate" "enddate" "categoryid2" "t" "cat" "category" "action" "bukva" "redirect_uri" "firstname" "c" "lastname" "uid" "startTime" "eventSearch" "categoryids2" "categoryids" "sort" "positiontitle" "groupid" "m" "message" "tag" "pn" "title" "orgId" "text" "handler" "myord" "myshownums" "id_site" "city" "search_query" "msg" "sortby" "produkti_po_cena" "produkti_po_ime" "mode" "CODE" "location" "v" "order" "n" "term" "start" "k" "redirect" "ref" "file" "mebel_id" "country" "from" "r" "f" "field5B5D" "searchScope" "state" "phone" "Itemid" "lng" "place" "bedrooms" "expand" "a" "price" "d" "path" "address" "day" "display" "error" "form" "language" "mls" "kw" "u" "/?s=" "/search?q=" "/index.php?lang=" "/pplay/info_prenotazioni.asp?immagine=" "/shared/lgflsearch.php?terms=" "/index.php?page=" "/search?query=" "/en/Telefon-Cam?search=" "/index.php?bukva=" "/pro/events_print_setup.cfm?list_type=" "/pro/events_print_setup.cfm?categoryid=" "/pro/events_print_setup.cfm?categoryid2=" "/?eventSearch=" "/?startTime=" "/pro/events_ical.cfm?categoryids=" "/pro/events_ical.cfm?categoryids2=" "/pro/events_print_setup.cfm?month=" "/pro/events_print_setup.cfm?year=" "/pro/events_print_setup.cfm?begindate=" "/pro/events_print_setup.cfm?enddate=" "/search?keyword=" "/?q=" "/search/?q=" "/index.php?pn=" "/?lang=" "/property/search?uid=" "/index.php?id=" "/search?orgId=" "/products?handler=" "/pro/events_print_setup.cfm?view=" "/pro/events_print_setup.cfm?keywords=" "/?p=" "/search.php?q=" "/?search=" "/pro/minicalendar_detail.cfm?list_type
+
+
