a89e639a46cd9efb9daab6c9c4cf201a4befda5d
Shreyash Khare <69174544+LaST0-0CoDe@users.noreply.github.com>
2023-03-17 13:50:57 +0530

    Update find_keywords.sh

--- find_keywords.sh
+++ find_keywords.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
-function banner(){
-	printf "\n${bgreen}"
-	printf "  ██▀███  ▓█████  ▄████▄   ▒█████   ███▄    █   █████▒▄▄▄█████▓ █     █░\n"
-	printf " ▓██ ▒ ██▒▓█   ▀ ▒██▀ ▀█  ▒██▒  ██▒ ██ ▀█   █ ▓██   ▒ ▓  ██▒ ▓▒▓█░ █ ░█░\n"
-	printf " ▓██ ░▄█ ▒▒███   ▒▓█    ▄ ▒██░  ██▒▓██  ▀█ ██▒▒████ ░ ▒ ▓██░ ▒░▒█░ █ ░█ \n"
-	printf " ▒██▀▀█▄  ▒▓█  ▄ ▒▓▓▄ ▄██▒▒██   ██░▓██▒  ▐▌██▒░▓█▒  ░ ░ ▓██▓ ░ ░█░ █ ░█ \n"
-	printf " ░██▓ ▒██▒░▒████▒▒ ▓███▀ ░░ ████▓▒░▒██░   ▓██░░▒█░      ▒██▒ ░ ░░██▒██▓ \n"
-	printf " ░ ▒▓ ░▒▓░░░ ▒░ ░░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░   ▒ ▒  ▒ ░      ▒ ░░   ░ ▓░▒ ▒  \n"
-	printf "   ░▒ ░ ▒░ ░ ░  ░  ░  ▒     ░ ▒ ▒░ ░ ░░   ░ ▒░ ░          ░      ▒ ░ ░  \n"
-	printf "   ░░   ░    ░   ░        ░ ░ ░ ▒     ░   ░ ░  ░ ░      ░        ░   ░  \n"
-	printf "    ░        ░  ░░ ░          ░ ░           ░                      ░    \n"
-	printf "                 ░                                                      \n"
-	printf " ${reconftw_version}                                 by @six2dez${reset}\n"
-}
-
 
 # Define the animation string
 animation="-\|/"
-
-# Start the loop
-while true; do
-    # Print the animation string with a message
-    printf "\r%s %s" "Your URL is loading" "${animation:i++%${#animation}:1}"
-    # Delay for a short time to slow down the animation
-    sleep 0.1
-done
 
 while getopts "u:" arg; do
   case $arg in
@@ -43,11 +20,25 @@ fi
 found_urls_file="found_urls.txt"
 touch $found_urls_file
 
+# Start the animation loop
+while true; do
+    # Print the animation string with a message
+    printf "\r%s %s" "Your URL is loading" "${animation:i++%${#animation}:1}"
+    # Delay for a short time to slow down the animation
+    sleep 0.1
+done & # Run the loop in the background
+
 # Loop through the URLs file and search for the specified keywords
 while read url; do
   if echo "$url" | grep -E 'q|s|search|id|lang|keyword|query|page|keywords|year|view|email|type|name|p|month|immagine|list_type|url|terms|categoryid|key|l|begindate|enddate|categoryid2|t|cat|category|action|bukva|redirect_uri|firstname|c|lastname|uid|startTime|eventSearch|categoryids2|categoryids|sort|positiontitle|groupid|m|message|tag|pn|title|orgId|text|handler|myord|myshownums|id_site|city|search_query|msg|sortby|produkti_po_cena|produkti_po_ime|mode|CODE|location|v|order|n|term|start|k|redirect|ref|file|mebel_id|country|from|r|f|field5B5D|searchScope|state|phone|Itemid|lng|place|bedrooms|expand|e|price|d|path|address|day|display|a|error|form|language|mls|kw|u' > /dev/null; then
     echo $url >> $found_urls_file
   fi
 done < $url_file
+
+# Stop the animation loop by killing the background process
+kill $! >/dev/null 2>&1
+
+# Clear the loading animation line
+printf "\r%s\r" ""
 
 echo "Keyword found URLs saved in $found_urls_file"
