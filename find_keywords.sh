470a0dbfb69b721eb5b1d40886b6aa1f31a562e3
Shreyash Khare <69174544+LaST0-0CoDe@users.noreply.github.com>
2023-03-17 12:50:52 +0530

    Update find_keywords.sh

--- find_keywords.sh
+++ find_keywords.sh
@@ -16,7 +16,12 @@ then
 fi
 
 # Define keywords to search for
-keywords=(q s search id lang keyword query page keywords year view email type name p month immagine list_type url terms categoryid key l begindate enddate categoryid2 t cat category action bukva redirect_uri firstname c lastname uid startTime eventSearch categoryids2 categoryids sort positiontitle groupid m message tag pn title orgId text handler myord myshownums id_site city search_query msg sortby produkti_po_cena produkti_po_ime mode CODE location v order n term start k redirect ref file mebel_id country from r f field5B5D searchScope state phone Itemid lng place bedrooms expand e price d path address day display a error form language mls kw u)
+keywords=(q= s= search= id= lang= keyword= query= page= keywords= year= view= email= type= name= p= month= immagine= list_type= url= terms= categoryid= key= l= begindate= enddate= categoryid2= t= cat= category= action= bukva= redirect_uri= firstname= c= lastname= uid= startTime= eventSearch= categoryids2= categoryids= sort= positiontitle= groupid= m= message= tag= pn= title= orgId= text= handler= myord= myshownums= id_site= city= search_query= msg= sortby= produkti_po_cena= produkti_po_ime= mode= CODE= location= v= order= n= term= start= k= redirect= ref= file= mebel_id= country= from= r= f= field5B5D= searchScope= state= phone= Itemid= lng= place= bedrooms= expand= e= price= d= path= address= day= display= a= error= form= language= mls= kw= u=
+
+)
+
+# Initialize list of URLs with keywords
+url_list=""
 
 # Read each URL from file and search for keywords
 while read url
@@ -24,12 +29,20 @@ do
     # Split URL by query string parameters
     params=$(echo "${url}" | awk -F'[?&]' '{print substr($0,index($0,$2))}')
 
-    # Search for each keyword in URL parameters
-    for keyword in "${keywords[@]}"
-    do
-        echo "${params}" | grep -o "${keyword}=[^&]*" | sed "s/${keyword}=//g" | while read value
+    # Check if URL contains at least one keyword
+    if echo "${params}" | grep -q -E "${keywords[*]}"
+    then
+        url_list="${url_list}${url}\n"
+        # Search for each keyword in URL parameters
+        for keyword in "${keywords[@]}"
         do
-            echo "Keyword ${keyword} found in ${url} with value ${value}"
+            echo "${params}" | grep -o "${keyword}[^&]*" | sed "s/${keyword}//g" | while read value
+            do
+                echo "Keyword ${keyword} found in ${url} with value ${value}"
+            done
         done
-    done
+    fi
 done < "${url_file}"
+
+# Output list of URLs with keywords
+echo -e "URLs with keywords:\n${url_list}"
